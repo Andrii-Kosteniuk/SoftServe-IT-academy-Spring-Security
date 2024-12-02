1ca91e8c053045a61e65f62825d00719ed6fd103
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-02 23:07:53 +0100

    Implemented security configuration. Enhanced apiFilterChain

--- src/main/java/com/softserve/itacademy/config/security/SecurityConfig.java
+++ src/main/java/com/softserve/itacademy/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -12,20 +13,33 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 // TODO: add required annotations to enable security
+//Done
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final WebAuthenticationProvider webAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(withDefaults())
-            .httpBasic(withDefaults());
+                .httpBasic(withDefaults());
         http.exceptionHandling(customizer -> customizer
-            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
 
         http.authorizeHttpRequests(authorize -> authorize
                 // TODO: open access to static resources, home and login pages
                 //       other pages - for authenticated only
-                .anyRequest().permitAll());
+                // Done
+                .requestMatchers("/static/**", "/img/**", "/login", "/home", "/").permitAll()
+                .requestMatchers("/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated());
+        http.authenticationProvider(webAuthenticationProvider);
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+        );
+
         return http.build();
     }
 }
